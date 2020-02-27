91a9c9ee3e2ac1e800434dbdbcc24b174ca2bde8
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-02-27 13:56:31 -0600

    created all the express route, now db stuff

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,18 @@
 module.exports = function(app) {
-    
+    app.get("/api/workouts", function(req, res){
+        console.log("get")
+    })
+
+    app.put("/api/workouts/:id", function(req, res){
+        console.log("put")
+        console.log(req.params.id)
+    })
+
+    app.post("/api/workouts", function(req, res){
+        console.log("post")
+    })
+
+    app.get("/api/workouts/range", function(req, res){
+        console.log("other get")
+    })
 }
